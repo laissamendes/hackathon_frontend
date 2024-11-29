05cad93b7d863568efbe609d7dcff675d1f4bd5d
DanielaVenturi <danielaventuri14@gmail.com>
2024-11-29 17:30:05 -0300

    Alteração do local da pasta plugins

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.mm6fot7e7j"
+    "revision": "0.u6f66dvdi7o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
